0447e325ada68efbbc7f278b95b3fc5a4c7959e1
zeb <zeb.7237@gmail.com>
2023-07-12 17:21:07 +0800

    first commit

---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,17 @@
+import { track, tirgger } from "./effect"
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      track(target, key)
+      return res
+    },
+    set(target, key, value) {
+      const res = Reflect.set(target, key, value)
+      trugger(target, key)
+      return res
+    }
+  })
+}
+
