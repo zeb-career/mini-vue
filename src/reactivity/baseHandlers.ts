ec88e291d071c91a7590b9b6c17d02ae9e299ac1
zeb <zeb.7237@gmail.com>
2023-07-13 17:12:07 +0800

    chore: add isReactive and isReadonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,11 +1,19 @@
 import { track, trigger } from "./effect"
+import { ReactiveFlags } from "./reactive"; './reactive'
 
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true)
 
 function createGetter(isReadonly = false) {
+
   return function get(target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    }
+
     const res = Reflect.get(target, key)
     if (!isReadonly)
       track(target, key)
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,13 +1,15 @@
-import { readonly } from "../reactive"
+import { readonly, isReadonly } from "../reactive"
 
 describe("readonly", () => {
-  it('happy path', () => {
+  it('make nested values readonly', () => {
     const original = {
       foo: 1, bar: { baz: 2 }
     }
     const wrapped = readonly(original)
     expect(wrapped).not.toBe(original)
     expect(wrapped.foo).toBe(1)
+    expect(isReadonly(wrapped)).toBe(true)
+    expect(isReadonly(original)).toBe(false)
   })
 
   it('warn then call set', () => {
