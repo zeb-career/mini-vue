0159fe6d0e42fd249126e51e2545d24268fb7f52
zeb <zeb.7237@gmail.com>
2023-07-13 16:33:33 +0800

    feat:add stop onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "../shared"
+
 class ReactiveEffect {
   private _fn: any
+  deps = []
+  active = true
+  onStop?: () => void
   constructor(fn, public scheduler?) {
     this._fn = fn
   }
@@ -8,6 +13,22 @@ class ReactiveEffect {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop)
+        this.onStop()
+      this.active = false
+    }
+
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 const targetMap = new Map()
@@ -23,7 +44,10 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep)
   }
+
+  if (!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -38,14 +62,19 @@ export function trigger(target, key) {
   }
 }
 
-
 let activeEffect;
 export function effect(fn, options: any = {}) {
-  const _effect = new ReactiveEffect(fn, options?.scheduler)
 
+  const _effect = new ReactiveEffect(fn, options?.scheduler)
+  extend(_effect, options)
   _effect.run();
 
-  const runner = _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect;
 
   return runner
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
