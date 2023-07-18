481084274c2e03fc2819cf9f805a1a0d04ccb6af
zeb <zeb.7237@gmail.com>
2023-07-18 15:53:50 +0800

    chore:add proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
-import { ref, isRef, unRef } from '../ref'
+import { ref, isRef, unRef, proxyRefs } from '../ref'
 
 describe('ref', () => {
   it('happy path', () => {
@@ -50,9 +50,32 @@ describe('ref', () => {
     expect(isRef(1)).toBe(false)
     expect(isRef(user)).toBe(false)
   })
+
   it('unRef', () => {
     const a = ref(1)
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
   })
+
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: "zhangshan"
+    }
+
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe("zhangshan")
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(10)
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
+
+  })
+
 })
